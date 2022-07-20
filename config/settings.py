8e8acf58b86b6505517cf476d75d1f7efb81fb54
Jjenny-K <kkjjh0129@gmail.com>
2022-07-20 16:59:30 +0900

    Feat: postgres DB settings

--- config/settings.py
+++ config/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASS'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
     }
 }
 
